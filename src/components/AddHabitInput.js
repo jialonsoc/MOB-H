d9ba38c5010ac45d2c3bbaaa61a295ad888a6e53
Jaime <jaime.alonso@uc.cl>
2024-11-17 18:26:06 -0300

    correcion agregar habito

--- src/components/AddHabitInput.js
+++ src/components/AddHabitInput.js
@@ -43,25 +43,15 @@ export default function AddHabitInput({ value, onChangeText, onSubmit }) {
 
     return (
         <View style={styles.container}>
-            <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-                <View style={styles.inputContainer}>
-                    <TextInput
-                        style={styles.input}
-                        value={habitName}
-                        onChangeText={setHabitName}
-                        placeholder="Añadir nuevo hábito"
-                        placeholderTextColor="#666"
-                        returnKeyType="done"
-                        onSubmitEditing={() => setShowModal(true)}
-                    />
-                    <TouchableOpacity 
-                        style={styles.addButton}
-                        onPress={() => setShowModal(true)}
-                    >
-                        <Ionicons name="add" size={24} color="#fff" />
-                    </TouchableOpacity>
-                </View>
-            </TouchableWithoutFeedback>
+            <TouchableOpacity 
+                style={styles.addButton}
+                onPress={() => {
+                    setShowModal(true);
+                    setHabitName('');
+                }}
+            >
+                <Ionicons name="add" size={24} color="#fff" />
+            </TouchableOpacity>
 
             <Modal
                 visible={showModal}
@@ -154,23 +144,9 @@ export default function AddHabitInput({ value, onChangeText, onSubmit }) {
 
 const styles = StyleSheet.create({
     container: {
-        flexDirection: 'row',
-        paddingHorizontal: 16,
+        marginHorizontal: 16,
         marginBottom: 16,
-    },
-    input: {
-        flex: 1,
-        height: 46,
-        backgroundColor: '#fff',
-        borderRadius: 23,
-        paddingHorizontal: 20,
-        marginRight: 10,
-        fontSize: 16,
-        shadowColor: '#000',
-        shadowOffset: { width: 0, height: 1 },
-        shadowOpacity: 0.1,
-        shadowRadius: 2,
-        elevation: 2,
+        position: 'relative',
     },
     addButton: {
         width: 46,
@@ -179,6 +155,7 @@ const styles = StyleSheet.create({
         backgroundColor: '#007AFF',
         justifyContent: 'center',
         alignItems: 'center',
+        alignSelf: 'flex-end',
         shadowColor: '#000',
         shadowOffset: { width: 0, height: 2 },
         shadowOpacity: 0.2,
@@ -292,9 +269,5 @@ const styles = StyleSheet.create({
         justifyContent: 'space-between',
         width: '100%',
         marginTop: 20,
-    },
-    inputContainer: {
-        flexDirection: 'row',
-        alignItems: 'center',
     },
 });
